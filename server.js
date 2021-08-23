b3db4604ce8511131c6a1d679d60d449800efc58
janhgit <janjakob124@gmail.com>
2021-08-23 19:29:31 +0200

    rmv

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 
-const spotifyClientID = "fa853e3f05bf4ca4a5b4ccd2ee49b065"
-const spotifyClientSecret = "8046e09f7a6b4a9c8ee5495b3194f0cc"
+
+
 
 const app = express();
 const port =  8000;
